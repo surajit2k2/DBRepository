fa3769b7ac76867297415e429f4614e49aaf4123
surajit2k2 <31662053+surajit2k2@users.noreply.github.com>
2019-02-20 15:49:49 +0530

    Update InsertData.sql

--- InsertData.sql
+++ InsertData.sql
@@ -1,2 +1,2 @@
-insert into Persons (PersonID,LastName,FirstName,Address,City) values(1,'Mukhopadhyay','Surajit','Kasba','Kolkata);
-insert into Persons (PersonID,LastName,FirstName,Address,City) values(1,'Ghosh','Anupom','Dunkuni','Kolkata);
+insert into Persons (PersonID,LastName,FirstName,Address,City) values(1,'Mukhopadhyay','Surajit','Kasba','Kolkata');
+insert into Persons (PersonID,LastName,FirstName,Address,City) values(2,'Ghosh','Anupom','Dunkuni','Kolkata');
